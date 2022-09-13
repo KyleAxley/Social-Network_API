0fcb3a4809dcf267b9bc1766844564085341ad6d
KyleAxley <kanderson7645@yahoo.com>
2022-09-12 22:39:47 -0400

    model structure started

---
+++ server.js
@@ -0,0 +1,20 @@
+//import for mongoose package
+const mongoose = require ('mongoose');
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 3001
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public'));
+
+app.use(require('./routes'));
+
+//connnection to mongoose route
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Social-Network_API', {
+    useNewURLParser: true,
+    useUnifiedTopology: true
+});
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
