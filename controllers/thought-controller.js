2f6ccfb9f92d0433f430382d826e43d0ce3fe734
KyleAxley <kanderson7645@yahoo.com>
2022-09-13 00:47:14 -0400

    added create thought and started TODO in routes

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,29 +1,59 @@
-const { thought } = require('../models'); 
+const { thought, user } = require("../models");
 
 const thoughtController = {
-    
-    // get all thoughts
-    getThought(req, res){
-        thought.find({})
-        .then((dbThoughtData) => res.json(dbThoughtData))
-        .catch((err) => {
-            console.log(err);
-            res.status(404).json(err);
-        })
-    },
-    
-    //get single thought
-    getThoughtById(){},
+  // get all thoughts
+  getThought(req, res) {
+    thought
+      .find({})
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => {
+        console.log(err);
+        res.status(404).json(err);
+      });
+  },
 
-    //create thought
-    createThought(){},
+  //get single thought
+  getThoughtById({ params }, res) {
+    thought
+      .findOne({ _id: params.id })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res
+            .status(404)
+            .json({ message: "There is no thought found with that id!" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.json(err));
+  },
 
-    //update thought
-    updateThought(){},
+  //create thought
+  createThought({ body }, res) {
+    thought.create(body).then((dbThoughtData) => {
+      return user.findOneAndUpdate(
+        { _id: body.userId },
+        { $push: { thoughts: thoughtData._id } },
+        { new: true }
+      );
+    })
+    .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res
+            .status(404)
+            .json({ message: "There is no thought found with that id!" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => res.json(err));
+  },
 
-    //delete thought
-    deleteThought(){},
+  //TODO:⬇️ update thought
+  updateThought() {},
+
+  //TODO: ⬇️delete thought
+  deleteThought() {},
 };
 
-
-module.exports = thoughtController; 
+module.exports = thoughtController;
---
+++ routes/api/thought-routes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+//TODO:⬇️ set up GET all and POST at /api/thoughts
+
+
+//TODO:⬇️ set up GET one, PUT, and DELETE at /api/thoughts/:id
+
+module.exports = router;
