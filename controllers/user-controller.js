118a2c4606b78288a42594f075a96e9705d99acb
KyleAxley <kanderson7645@yahoo.com>
2022-09-12 23:51:19 -0400

    change user to lowercase

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,7 +4,7 @@ const userController = {
   
     //get all users
   getUser(req, res) {
-    User.find({})
+    user.find({})
     .then((dbUserData) => res.json(dbUserData))
     .catch((err) => {
         console.log(err);
@@ -14,7 +14,7 @@ const userController = {
 
   //get single user by id
   getUserById({ params }, res) {
-    User.findOne({ _id: params.id })
+    user.findOne({ _id: params.id })
     .then((dbUserData) => {
         //if no user found send 404
         if(!dbUserData) {
@@ -31,14 +31,14 @@ const userController = {
 
   //create a user
   createUser({body}, res) {
-    User.create(body)
+    user.create(body)
         .then((dbUserData) => res.json(dbUserData))
         .catch((err) => res.status(400).json(err)); 
   },
 
   //update user
   updateUser({ params, body }, res) {
-    User.findOneAndUpdate({ _id: params.id}, body, { new: true })
+    user.findOneAndUpdate({ _id: params.id}, body, { new: true })
     .then((dbUserData) => {
         if(!dbUserData) {
             res.status(404).json({ messae: 'There is no user found by this id!'});
@@ -51,7 +51,7 @@ const userController = {
 
   //delete a user
   deleteUser({params}, res) {
-    User.findOneAndDelete({ _id: params.id})
+    user.findOneAndDelete({ _id: params.id})
     .then((dbUserData) => {
         if(!dbUserData) {
             res.status(404).json({ messae: 'There is no user found by this id!'});
